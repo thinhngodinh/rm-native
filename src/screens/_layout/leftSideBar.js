d52d3ab979d15c30d0ce23dc16e11b50e1dd759d
Thinh Ngo <thinh.ngodinh@elinext.com>
2019-01-07 19:20:58 +0700

    thinhnd - #18801 Create Application Layout -  style menu sidebar

--- src/screens/_layout/leftSideBar.js
+++ src/screens/_layout/leftSideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollView, ImageBackground, StyleSheet } from 'react-native';
 import { DrawerItems, SafeAreaView } from 'react-navigation';
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { Button, Icon, Text } from 'native-base';
+import { Button, Icon, Text, Thumbnail } from 'native-base';
 
 
 import { connect } from 'react-redux';
@@ -28,33 +28,42 @@ class SideBar extends React.Component {
 
     render() {
         const { props } = this;
-
+        console.log(props.user)
         return (
-            <ScrollView contentContainerStyle={{flex: 1}}>
+            <ScrollView contentContainerStyle={{ flex: 1 }}>
                 <SafeAreaView style={styles.container} forceInset={{ top: 'never', horizontal: 'never' }}>
-                    <Grid style={{justifyContent: 'space-around'}}>
-                        <ImageBackground
-                            source={require('../../assets/login_bg.jpg')}
-                            blurRadius={1}
-                            style={{height: 150}}>
-                            <Row>
-                            </Row>
-                            <Row style={{justifyContent: 'space-around'}}>
-                                {props.user && <Text style={{color: '#ffffff'}}>{props.user.first_name} {props.user.last_name}</Text>}
-                                <Button 
-                                    onPress={this._dispatchLogout} >
-                                    <Icon name='md-power' />
-                                </Button>
-                            </Row>
-                        </ImageBackground>
-                        <Row style={{flexGrow: 9}}>
-                            <DrawerItems
-                                activeBackgroundColor='#ef2f16'
-                                labelStyle={{color: '#ffffff'}}
-                                itemsContainerStyle={{width: '100%'}}
-                                { ...props} />
+                    <ImageBackground
+                        source={require('../../assets/login_bg.jpg')}
+                        blurRadius={1}
+                        style={{ height: 160 }}>
+                        <Row>
                         </Row>
-                    </Grid>
+                        <Row style={{ justifyContent: 'space-around' }}>
+                            <Col size={7} style={{padding: 5}}>
+                                {props.user && <Text style={{ color: '#ffffff' }}>{props.user.first_name} {props.user.last_name}</Text>}
+                                {props.user && <Text style={{ color: '#d1d1d1', fontSize: 13, fontWeight: '100' }}>{props.user.email}</Text>}
+                                {props.user && <Text style={{ color: '#d1d1d1', fontSize: 13 }}> ID #{props.user.id}</Text>}
+                            </Col>
+                            <Col size={3} style={{padding: 5}}>
+                                {props.user && <Thumbnail size={15} square source={{ uri: props.user.avatar }} style={{ borderRadius: 3 }}></Thumbnail>}
+                            </Col>
+                        </Row>
+                    </ImageBackground>
+                    <DrawerItems
+                        activeBackgroundColor='#ef2f16'
+                        labelStyle={{ color: '#ffffff' }}
+                        itemsContainerStyle={{ width: '100%' }}
+                        {...props} />
+                    <Button
+                        bordered
+                        block
+                        danger
+                        onPress={this._dispatchLogout}
+                        style={{margin: 10, marginTop: 30}}>
+                        <Icon name='md-power' />
+                        <Text>Logout</Text>
+                    </Button>
+                    
                 </SafeAreaView>
             </ScrollView>
         );
