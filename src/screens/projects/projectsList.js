ef9a438f697d121b3995d0ebf2cb0c80f9b01b7c
Anhtx <anh.truongxuan@elinext.com>
2019-01-22 17:22:03 +0700

    anhtx - update #18889 style no project found

--- src/screens/projects/projectsList.js
+++ src/screens/projects/projectsList.js
@@ -176,11 +176,12 @@ class ProjectsScreen extends React.Component {
                                 color={loadingData ? '#04b6fe' : '#fff'} />
                         }
                         {projectListData && projectListData.total_pages > 0 && (projectListData.paged === projectListData.total_pages) &&
-                            <Text style={{ color: '#00000050' }}>All Projects are loaded.</Text>
+                            <Text style={{ color: '#00000050', paddingLeft: 16, paddingRight: 16 }}>All Projects are loaded.</Text>
                         }
                         {projectListData && projectListData.projects.length == 0 && !loadingData &&
-                            <View style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row'}}>
-                                <Text>No Projects</Text>
+                            <View style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row', flexWrap: 'wrap'}}>
+                                <Icon name='md-folder-open' style={{fontSize: 50, color: '#77777770'}}></Icon>
+                                <Text style={{display: 'flex', width: '100%', textAlign: 'center', color: '#77777770'}}>No Project Found</Text>
                             </View>
                         }
                     </View>
